9288ba6822fb7ec59d24eb8ef93203d19538a427
cristinasavca27 <89223929+cristinasavca27@users.noreply.github.com>
2021-08-20 00:08:57 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,33 @@
+
+
+function sendEmail() {
+    var nume=document.getElementById("nume").value;
+    var mesaj=document.getElementById("mesaj").value;
+    window.open('mailto:frunza.elena@edu.md?subject=' +   nume +  '&body=' + mesaj);
+  }
+
+  function data() {
+    var date = new Date();
+    var an, luna, zi;
+    var text;
+
+    zi = date.getDate();
+    luna = date.getMonth() + 1;
+    an = date.getFullYear();
+
+    text = zi + "-" + luna + "-" + an;
+    alert(text);
+
+    document.getElementById("text-data").innerHTML = "Cîrnățenii Noi,  " + text;
+}
+
+
+
+function mobileMenu() {
+    var x = document.getElementById("navbar");
+    if (x.className === "nav") {
+        x.className += " mobile";
+    } else {
+        x.className = "nav";
+    }
+}
